7cdc18d5ab1b1b966ace9e4b005987d78260ec45
Dae-Iddings <115945028+Dae-Iddings@users.noreply.github.com>
2022-12-26 15:54:29 -0600

    creates template for html

--- myScript.js
+++ myScript.js
@@ -1,22 +1,47 @@
+
+let story = {
+    currentScene: "attack",
+    attack: {
+        title: 'Chapter One',
+        story: 'You are a journalist sitting at your desk coved notes, thinking abut how it has been three months since your last big scoop, the interview of Instagram scammer Loretta Lasher. Your thoughts are interrupted the sound of your phone ringing. It\'s your cousin Julie.',
+        dialogue: '"Hello y/n. I need your help a witch visited me last night and told me I had five days to find her beloved or else. Can you please come help me investigate?"',
+        choices: [
+            {
+                choice: 'Of course I\'ll help you',
+            },
+            {
+                choice: 'No theres a story of a cat stuck in a tree I have to cover'
+            }
+        ]
+
+
+    }
+};
+
 document.addEventListener('DOMContentLoaded', function() {
     let button = document.querySelector('#start-button');
     let input = document.querySelector('#name-input');
     let content = document.querySelector('#content');
     button.addEventListener('click', function() {
      content.innerHTML = `
-     <h1>Chapter Two</h2>
-     <p>You are a journalist sitting at your desk coved notes, thinking abut how it has been three months since your last big scoop, the interview of Instagram scammer Loretta Lasher. Your thoughts are interrupted the sound of your phone ringing. It's your cousin Julie.</p>
-     <p><i>'Hello ${input.value}. I need your help a witch visited me last night and told me I had five days to find her beloved or else. Can you please come help me investigate?'</i></p>
-     <div>
-     <input id='radio1' type='radio' name='choices'/>  
-     <label for='radio1'>Of course I'll help you</label>
-     </div>
-     <div>
-      <input id='radio2' type='radio' name='choices'/> 
-     <label for='radio2'>No theres a story of a cat stuck in a tree I have to cover</label>
-     <div>
+     <h1>${story[story.currentScene].title}</h1>
+     <p>${story[story.currentScene].story}</p>
+     <p><i>${story[story.currentScene].dialogue}</i></p>
+     ${getInputs()}
      <button>Continue</button>
       `
     })
 })
-   
+
+  function getInputs() {
+    let input = '';
+    for (let i = 0; i < story[story.currentScene].choices.length; i++) {
+        input+=`
+        <div>
+        <input id='radio${i}' type='radio' name='choices'/>  
+        <label for='radio${i}'>${story[story.currentScene].choices[i].choice}</label>
+        </div>
+        `
+    }
+    return input;
+  } 
