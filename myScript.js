a88f7448a52e1321a22d6e2dc2caf452df227baf
Dae-Iddings <115945028+Dae-Iddings@users.noreply.github.com>
2022-12-27 11:38:10 -0600

    adds chap one  kindness and chap one game over

--- myScript.js
+++ myScript.js
@@ -3,8 +3,8 @@ let story = {
     currentScene: "attack",
     attack: {
         title: 'Chapter One',
-        story: 'Y/N, you awake in the middle of the night to a low rumbling. It increases as a women dressed in a white 19th century nightgown rises from your floor. You immediately recognize her as the Bell Witch a witch said to haunt your town as far back as 1817. The rumbling stops as she begins to cry out. Her voice surrounds you.',
-        dialogue: '"Betty? Oh where if my Betty? Please help me find my Betty."',
+        story: 'Y/N, you awake in the middle of the night to a low rumbling. It increases as a women dressed in a white 19th century nightgown rises from your floor. You immediately recognize her as the Bell Witch a witch said to haunt your town as far back as 1817. The rumbling stops as she begins pacing unaware of your presents and crys out. Her voice surrounds you.',
+        dialogue: '"Betty? Oh where if my Betty? Please help me find my Betty. I know John Bell stole my betty"',
         choices: [
             {
                 choice: 'Offer to help',
@@ -18,7 +18,28 @@ let story = {
 
 
     },
-
+help: {
+    title: 'Chapter One: Choose Kindness',
+    story: 'She stops in her tracks and turns to you. Before disappearing and says',
+    dialogue: '"The secrets to where my Betty is lie in John Bell\'s journal find it before its too late."',
+    choices: [
+        {
+        choice: 'Continue to the next morning',
+        destination: 'nextMorning'
+        }
+    ]
+},
+scream: {
+    title: 'Game Over',
+    story: 'The witch turns to you with rage in her eyes. Her voices turns to screams so loud they drown out your own. Your house begins to shake.',
+    dialogue: '"You\'re working with him? You Helped him hurt my Betty. You will Pay"',
+    choices: [
+        {
+            choice: 'Game Over! Click to restart',
+            destination: 'attack'
+        }
+    ]
+}
 };
 
 document.addEventListener('DOMContentLoaded', function () {
